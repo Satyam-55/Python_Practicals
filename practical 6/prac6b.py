6614ba8a9f60213a1d374be4aae9fca6b33dc628
Satyam-55 <89448035+Satyam-55@users.noreply.github.com>
2021-10-05 00:50:40 +0530

    Add files via upload

---
+++ practical 6/prac6b.py
@@ -0,0 +1,4 @@
+#def main():
+f=open("practical 6/prac6b.txt","a+")
+f.write("\nPython is awesome\n")
+f.close()
---
+++ practical 9/prac_9a1.py
@@ -0,0 +1,8 @@
+import tkinter
+from tkinter import *
+window =tkinter.Tk()
+O=Canvas(window,bg="red",width=400,height=240)
+O.pack()
+n=Label(window,text="Hello World!")
+n.pack()
+window.mainloop()
